a563e02afa368e3654b9f1341af76d3a12a94075
Ali HADDANI <al-had@hotmail.com>
2022-09-19 04:23:26 +0100

    push

--- src/views/ContentFeed.vue
+++ src/views/ContentFeed.vue
@@ -134,10 +134,10 @@ export default {
         return {
           dataPublish:"",
            events: [
-                {status: 'Ordered', date: '15/10/2020 10:30', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg'},
-                {status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7'},
-                {status: 'Shipped', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800'},
-                {status: 'Delivered', date: '16/10/2020 10:00', icon: 'pi pi-check', color: '#607D8B'}
+                {status: 'Commentaire Lorem ipsum', date: '15/10/2020 10:30', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg'},
+                {status: 'Commentaire Lorem ipsum Lorem ipsum Lorem ipsum', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7'},
+                {status: 'Commentaire', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800'},
+                {status: 'Commentaire Lorem ipsum Lorem ipsum Lorem ipsum', date: '16/10/2020 10:00', icon: 'pi pi-check', color: '#607D8B'}
             ],
 
         }
